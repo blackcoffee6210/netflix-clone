e88d6f6332d2770c8c423c4a0d247575d85fba39
blackcoffee6210 <blackcoffee6210@gmail.com>
2024-07-30 21:53:38 +0900

    header作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,25 @@
 import "./App.css";
 import { Banner } from "./components/Banner";
+import { Header } from "./components/Header";
 import { Row } from "./components/Row";
 import { requests } from "./request";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row
-        title="Trand Movies"
-        fetchUrl={requests.fetchTrending}
-      />
-      <Row
-        title="Action Movies"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="News Movies"
-        fetchUrl={requests.fetchNewsMovies}
-      />
-      <Row
-        title="Kids Movies"
-        fetchUrl={requests.fetchKidsMovies}
-      />
-      <Row
-        title="Romance Movies"
-        fetchUrl={requests.fetchRomanceMovies}
-      />
-      <Row
-        title="Documentaries"
-        fetchUrl={requests.fetchDocumentMovies}
-      />
+      <Row title="Trand Movies" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="News Movies" fetchUrl={requests.fetchNewsMovies} />
+      <Row title="Kids Movies" fetchUrl={requests.fetchKidsMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
